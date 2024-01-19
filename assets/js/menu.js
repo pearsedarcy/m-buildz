a1431202120a88c23f2709499fa7d142054a8352
pearsedarcy <pearse.darcy@outlook.com>
2024-01-19 10:33:59 +0100

    Add menu.js file and include it in index.html

---
+++ assets/js/menu.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("ul#menu li a").forEach((menu) => {
+    menu.addEventListener("click", function () {
+      document.getElementById("nav-toggle-box").checked = false;
+    });
+  });
+});
